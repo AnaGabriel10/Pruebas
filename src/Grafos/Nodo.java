a6ac3d190888d532a1b572a7f31a9340096345e7
marcelino <marcelino@DESKTOP-ULETCN4>
2021-11-17 02:17:22 -0600

    Se agrego la clase lienzo

--- src/Grafos/Nodo.java
+++ src/Grafos/Nodo.java
@@ -5,10 +5,23 @@
  */
 package Grafos;
 
+import java.awt.Graphics;
+
 /**
  *
  * @author marcelino
  */
 public class Nodo {
     
+    private int x, y;
+    public static final int d=60;
+
+    public Nodo(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+ 
+   public void pintar (Graphics g){
+       g.drawOval(this.x - d/2, this.y - d/2, d,d);
+   }
 }
